edc497a3f6c9bad637c2eb34eaeaa06882e9c8ea
Sem Schilder <sschilder@transip.nl>
2020-03-18 22:31:13 +0100

    city is used, but is not always present. Add a default

--- index.php
+++ index.php
@@ -2,15 +2,14 @@
 	require("header.php");
 	
 	$openRequests = array();
-	foreach($HeldNodig->getOpenRequests($_GET['city']) as $r){
+	foreach($HeldNodig->getOpenRequests($_GET['city'] ?? '') as $r){
 		$x = $r->getTwig();
 		$x['Category'] = $r->getCategory()->getName();
-		
-		
-		array_push($openRequests, $x);
+
+		$openRequests[] = $x;
 	}
 	$closedRequests = array();
-	foreach($HeldNodig->getClosedRequests($_GET['city']) as $r){
+	foreach($HeldNodig->getClosedRequests($_GET['city'] ?? '') as $r){
 		$x = $r->getTwig();
 		$x['Category'] = $r->getCategory()->getName();
 		
@@ -18,5 +17,4 @@
 		array_push($closedRequests, $x);
 	}
 			
-	echo $twig->render('homepage.html', ["openRequests"=>$openRequests,"closedRequests"=>$closedRequests,"city"=>$_GET['city']]);
-?>
+	echo $twig->render('homepage.html', ["openRequests"=>$openRequests,"closedRequests"=>$closedRequests,"city"=>$_GET['city'] ?? '']);
