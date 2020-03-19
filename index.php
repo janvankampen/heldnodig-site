642f73d93fbdbe8cb996770ab4eef920ac6005ca
Sem Schilder <sschilder@transip.nl>
2020-03-19 21:58:10 +0100

    city is used, but is not always present. Add a default (#9)
    * city is used, but is not always present. Add a default
    
    * Add some additional code style changes
    
    * Delete .env

--- index.php
+++ index.php
@@ -2,20 +2,22 @@
 	require("header.php");
 	
 	$openRequests = array();
-	foreach($HeldNodig->getOpenRequests($_GET['city']) as $r){
+	foreach($HeldNodig->getOpenRequests($_GET['city'] ?? '') as $r){
 		$x = $r->getTwig();
 		$x['Category'] = $r->getCategory()->getName();
-		
-		
-		array_push($openRequests, $x);
+
+		$openRequests[] = $x;
 	}
 	$closedRequests = array();
-	foreach($HeldNodig->getClosedRequests($_GET['city']) as $r){
+	foreach($HeldNodig->getClosedRequests($_GET['city'] ?? '') as $r){
 		$x = $r->getTwig();
 		$x['Category'] = $r->getCategory()->getName();
 		
-		
-		array_push($closedRequests, $x);
+		$closedRequests[] = $x;
 	}
-			
-	echo $twig->render('homepage.html', ["openRequests"=>$openRequests,"closedRequests"=>$closedRequests,"city"=>$_GET['city']]);
+
+  echo $twig->render('homepage.html', [
+    'openRequests' => $openRequests,
+    'closedRequests' => $closedRequests,
+    'city' => $_GET['city'] ?? ''
+  ]);
